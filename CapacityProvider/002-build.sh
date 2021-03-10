9a02e7c8774c8f2d226619070160fbf6337699e3
awsandy <awsandy@amazon.co.uk>
2021-03-10 12:57:21 +0000

    up

--- CapacityProvider/002-build.sh
+++ CapacityProvider/002-build.sh
@@ -1,3 +1,11 @@
+##
+# Empty prevos cloudstack bucket
+# aws cloudformation delete-stack   --stack-name CDKToolkit
+# delete stack CDKToolkit   ^^ need to empty buck first
+#
+#
+aws cloudformation delete-stack   --stack-name CDKToolkit
+aws cloudformation delete-stack   --stack-name ecsworkshop-base
 aws iam get-role --role-name "AWSServiceRoleForECS" || aws iam create-service-linked-role --aws-service-name "ecs.amazonaws.com"
 cd ~/environment
 git clone https://github.com/brentley/container-demo
