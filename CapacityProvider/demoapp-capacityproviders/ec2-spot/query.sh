4716d7dcf86723984127837b5d24978c4cfce1f4
awsandy <awsandy@amazon.co.uk>
2021-03-12 17:12:17 +0000

    up

---
+++ CapacityProvider/demoapp-capacityproviders/ec2-spot/query.sh
@@ -0,0 +1,6 @@
+aws ecs describe-container-instances --cluster container-demo \
+    --container-instances $(aws ecs list-container-instances \
+        --cluster container-demo --query 'containerInstanceArns[]' --output text) \
+                --query 'containerInstances[].{InstanceId: ec2InstanceId, 
+                    CapacityProvider: capacityProviderName, RunningTasks: runningTasksCount}' \
+                --output table
