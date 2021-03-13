a6f2e31d8312f61351b1467e1915f6535805c0c7
awsandy <awsandy@amazon.co.uk>
2021-03-13 15:10:26 +0000

    up

---
+++ CapacityProvider/011-deploy-demo-app-20.sh
@@ -0,0 +1,4 @@
+echo "Deploy app with 20x tasks"
+cd ~/environment/ecs-workshop/CapacityProvider/demoapp-capacityproviders/ec2-spot
+cp app.py.20 app.py
+cdk context --clear && cdk deploy --require-approval never
