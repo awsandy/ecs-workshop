7a9d4766ccd39d7b252a8d0f06a1d566bb9f3f8d
awsandy <awsandy@amazon.co.uk>
2021-03-13 12:07:59 +0000

    up

--- CapacityProvider/999-destroy.sh
+++ CapacityProvider/999-destroy.sh
@@ -1,2 +1,6 @@
 cd ~/environment/ecs-workshop/CapacityProvider/ecsdemo-capacityproviders/fargate
-cdk destroy -f
+cdk destroy
+cd ~/environment/ecs-workshop/CapacityProvider/demoapp-capacityproviders/ec2-spot
+cdk destroy
+cd ~/environment/ecs-workshop/CapacityProvider/cdk-ecs-infrastructure/cdk-asg_spot
+cdk destroy
