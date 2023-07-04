0a9fb519b7dabf0c3be3f5957a11f540ae66e469
awsandy <awsandy@amazon.co.uk>
2023-07-04 12:21:32 +0100

    up

--- mesh-microservices/00-save-orig.sh
+++
@@ -1,6 +0,0 @@
-files=( "ecsdemo-crystal" "ecsdemo-nodejs" "ecsdemo-frontend" "ecsdemo-platform" )
-for i in "${files[@]}"
-do
-cp ~/environment/${i}/cdk/app.py ~/environment/${i}/cdk/app.py.orig
-done
-
