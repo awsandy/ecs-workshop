e0e0e37e03893643f18143868d2049b018e19447
awsandy <awsandy@amazon.co.uk>
2023-07-04 13:35:05 +0100

    up

--- mesh-microservices/02-setup-mesh.sh
+++ mesh-microservices/02-setup-mesh.sh
@@ -3,7 +3,6 @@
 echo "Mesh edits"
 
 # platform
-cd ~/environment/ecsdemo-platform/cdk
 sed -i -e '/self.appmesh()/s/# //' ~/environment/ecsdemo-platform/cdk/app.py
 
 ## Crystal
@@ -11,22 +10,25 @@ echo "Crystal"
 
 lines=$(grep -Fn '#appmesh-proxy-uncomment' ~/environment/ecsdemo-crystal/cdk/app.py)
 unstart=$(echo $lines | awk '{print $1}' | cut -f1 -d':')
-unend=$(echo $lines | awk '{print $3}' | cut -f1 -d':')
-comm=$(printf "sed -i '%s,%s s/#//' ~/environment/ecsdemo-crystal/cdk/app.py" $unstart $unend)
-echo $comm
-eval $comm 
+if [[ $unstart != "" ]]; then
+    unend=$(echo $lines | awk '{print $3}' | cut -f1 -d':')
+    comm=$(printf "sed -i '%s,%s s/#//' ~/environment/ecsdemo-crystal/cdk/app.py" $unstart $unend)
+    echo $comm
+    eval $comm
+fi
 echo "self edit"
 sed -i -e "/self.appmesh()/s/# //" ~/environment/ecsdemo-crystal/cdk/app.py
-
 
 # nodejs
 echo "nodejs"
 lines=$(grep -Fn '# appmesh-proxy-uncomment' ~/environment/ecsdemo-nodejs/cdk/cdk/nodejsservice.py)
 unstart=$(echo $lines | awk '{print $1}' | cut -f1 -d':')
-unend=$(echo $lines | awk '{print $3}' | cut -f1 -d':')
-comm=$(printf "sed -i '%s,%s s/#//' ~/environment/ecsdemo-nodejs/cdk/cdk/nodejsservice.py" $unstart $unend)
-echo $comm
-eval $comm
+if [[ $unstart != "" ]]; then
+    unend=$(echo $lines | awk '{print $3}' | cut -f1 -d':')
+    comm=$(printf "sed -i '%s,%s s/#//' ~/environment/ecsdemo-nodejs/cdk/cdk/nodejsservice.py" $unstart $unend)
+    echo $comm
+    eval $comm
+fi
 
 echo "self edit"
 sed -i -e "self.appmesh()/s/# //" ~/environment/ecsdemo-nodejs/cdk/cdk/nodejsservice.py
@@ -36,12 +38,6 @@ echo "frontend"
 # frontend
 
 echo "Commenting previous frontend class"
-sed -i -e '/FrontendService(app, stack_name, env=_env)/s/^#*/#/' ~/environment/ecsdemo-frontend/cdk/app.py 
+sed -i -e '/FrontendService(app, stack_name, env=_env)/s/^#*/#/' ~/environment/ecsdemo-frontend/cdk/app.py
 echo "Uncommenting new frontend class"
-sed -i -e "/FrontendServiceMesh(app, stack_name, env=_env)/s/# //" ~/environment/ecsdemo-frontend/cdk/app.py 
-
-
-
-
-
-
+sed -i -e "/FrontendServiceMesh(app, stack_name, env=_env)/s/# //" ~/environment/ecsdemo-frontend/cdk/app.py
