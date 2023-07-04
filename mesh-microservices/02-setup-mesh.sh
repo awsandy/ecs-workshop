7757630574cabd7ef45e06c2d905302f079f8167
awsandy <awsandy@amazon.co.uk>
2023-07-04 13:36:52 +0100

    up

--- mesh-microservices/02-setup-mesh.sh
+++ mesh-microservices/02-setup-mesh.sh
@@ -10,8 +10,8 @@ echo "Crystal"
 
 lines=$(grep -Fn '#appmesh-proxy-uncomment' ~/environment/ecsdemo-crystal/cdk/app.py)
 unstart=$(echo $lines | awk '{print $1}' | cut -f1 -d':')
-if [[ $unstart != "" ]]; then
-    unend=$(echo $lines | awk '{print $3}' | cut -f1 -d':')
+unend=$(echo $lines | awk '{print $3}' | cut -f1 -d':')
+if [[ "$unend" != "" ]]; then
     comm=$(printf "sed -i '%s,%s s/#//' ~/environment/ecsdemo-crystal/cdk/app.py" $unstart $unend)
     echo $comm
     eval $comm
@@ -23,8 +23,8 @@ sed -i -e "/self.appmesh()/s/# //" ~/environment/ecsdemo-crystal/cdk/app.py
 echo "nodejs"
 lines=$(grep -Fn '# appmesh-proxy-uncomment' ~/environment/ecsdemo-nodejs/cdk/cdk/nodejsservice.py)
 unstart=$(echo $lines | awk '{print $1}' | cut -f1 -d':')
-if [[ $unstart != "" ]]; then
-    unend=$(echo $lines | awk '{print $3}' | cut -f1 -d':')
+unend=$(echo $lines | awk '{print $3}' | cut -f1 -d':')
+if [[ $unund != "" ]]; then
     comm=$(printf "sed -i '%s,%s s/#//' ~/environment/ecsdemo-nodejs/cdk/cdk/nodejsservice.py" $unstart $unend)
     echo $comm
     eval $comm
