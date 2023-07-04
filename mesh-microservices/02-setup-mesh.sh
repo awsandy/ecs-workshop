937534f74cf00a1f5aef790726e45bbe302bf081
awsandy <awsandy@amazon.co.uk>
2023-07-04 12:22:54 +0100

    up

--- mesh-microservices/02-setup-mesh.sh
+++ mesh-microservices/02-setup-mesh.sh
@@ -7,6 +7,7 @@ cd ~/environment/ecsdemo-platform/cdk
 sed -i -e '/self.appmesh()/s/# //' ~/environment/ecsdemo-platform/cdk/app.py
 
 ## Crystal
+echo "Crystal"
 
 lines=($(grep -Fn '#appmesh-proxy-uncomment' ~/environment/ecsdemo-crystal/cdk/app.py | cut -f1 -d:))
 unstart=$((${lines[0]} + 1))
@@ -16,13 +17,17 @@ sed -i -e "/self.appmesh()/s/# //" ~/environment/ecsdemo-crystal/cdk/app.py
 
 
 # nodejs
+echo "nodejs"
 lines=($(grep -Fn '#appmesh-proxy-uncomment' ~/environment/ecsdemo-nodejs/cdk/app.py | cut -f1 -d:))
 unstart=$((${lines[0]} + 1))
 unend=$((${lines[1]} - 1))
 sed -i "${unstart},${unend} s/# //" ~/environment/ecsdemo-nodejs/cdk/app.py 
-sed -i -e '/self.appmesh()/s/# //' ~/environment/ecsdemo-nodejs/cdk/app.py
+sed -i -e '"self.appmesh()/s/# //" ~/environment/ecsdemo-nodejs/cdk/app.py
+
+echo "frontend"
 
 # frontend
+
 #Commenting previous class
 sed -i -e '/FrontendService(app, stack_name, env=_env)/s/^#*/#/' ~/environment/ecsdemo-frontend/cdk/app.py 
 #Uncommenting new class
