c67139c67071658a640ff7030c62233dc6137baa
awsandy <awsandy@amazon.co.uk>
2023-07-04 12:01:11 +0100

    up

--- mesh-microservices/00-save-orig.sh
+++ mesh-microservices/00-save-orig.sh
@@ -1,5 +1,6 @@
 files=( "ecsdemo-crystal" "ecsdemo-nodejs" "ecsdemo-frontend" "ecsdemo-platform" )
 for i in "${files[@]}"
-cp ~/environment/${i}/cdk/app.py ~/environment/${i}/cdk/app.py.orig
 do
+cp ~/environment/${i}/cdk/app.py ~/environment/${i}/cdk/app.py.orig
+done
 
